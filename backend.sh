c87847e42d52b48b68c911677e51bb2444c3e295
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-09-10 11:21:00 +0530

    expense scripts

--- backend.sh
+++ backend.sh
@@ -47,5 +47,6 @@ dnf install mysql -y &>>$log_file
 stat_check
 
 echo load schema
+mysql_root_password=$1
 mysql -h 2.malleswaridevops.online -uroot -p$mysql_root_password < /app/schema/backend.sql &>>$log_file
 stat_check
