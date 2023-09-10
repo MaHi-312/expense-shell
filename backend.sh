d2e81f93817f4b2486f33b6161f01b989503b73c
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-09-10 10:21:55 +0530

    expense scripts

--- backend.sh
+++ backend.sh
@@ -1,50 +1,31 @@
 source common.sh
 component=backend
-
+type npm &>>$log_file
+if [ $? -ne 0 ];then
 echo install nodejs repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
-if [ $? =eq 0 ] ; then
-  echo -e "\e[32msucces\e[0m"
-  else
-    echo -e "\e[31mfailed\e[0m"
-    exit
-    fi
-
+stat_check
 
 echo install nodejs
 dnf install nodejs -y >>$log_file
-if [ $? =eq 0 ] ; then
-  echo -e "\e[32msucces\e[0m"
-    else
-      echo -e "\e[31mfailed\e[0m"
-      exit
-      fi
+stat_check
+fi
 
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-if [ $? =eq 0 ] ; then
-  echo -e "\e[32msucces\e[0m"
-    else
-      echo -e "\e[31mfailed\e[0m"
-      exit
-      fi
+stat_check
 
 echo add application user
-useradd expense &>>$log_file
-if [ $? =eq 0 ] ; then
-  echo -e "\e[32msucces\e[0m"
-    else
-      echo -e "\e[31mfailed\e[0m"
-      exit
-      fi
+
+id expense &>>$log_file
+if [ $? -ne 0 ];then
+  useradd expense &>>$log_file
+  fi
+stat_check
 
 echo clean app content
 rm -rf /app &>>$log_file
-if [ $? =eq 0 ] ; then
-  echo succes
-  else
-    echo failed
-    fi
+stat_check
 
 mkdir /app
 cd /app
@@ -53,34 +34,18 @@ download_and_extract
 
 echo download dependinces
 npm install &>>$log_file
-if [ $? =eq 0 ] ; then
-  echo succes
-  else
-    echo failed
-    fi
+stat_check
 
 echo start backend service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
-if [ $? =eq 0 ] ; then
-  echo succes
-  else
-    echo failed
-    fi
+stat_check
 
 echo install mysql client
 dnf install mysql -y &>>$log_file
-if [ $? =eq 0 ] ; then
-  echo succes
-  else
-    echo failed
-    fi
+stat_check
 
 echo load schema
 mysql -h 2.malleswaridevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-if [ $? =eq 0 ] ; then
-  echo succes
-  else
-    echo failed
-    fi
+stat_check
--- common.sh
+++ common.sh
@@ -8,5 +8,13 @@ echo $?
   echo extracting $component code
   unzip /tmp/$component.zip >>$log_file
   echo $?
-
 }
+
+ stat_check() {
+if [ $? =eq 0 ] ; then
+  echo -e "\e[32msucces\e[0m"
+  else
+    echo -e "\e[31mfailed\e[0m"
+    exit 1
+    fi
+ }
