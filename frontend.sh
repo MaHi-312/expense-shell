cf3a86ce35a2ba4867d1e72f1e5cd5a03da88d27
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-09-10 10:26:37 +0530

    expense scripts

--- frontend.sh
+++ frontend.sh
@@ -3,23 +3,21 @@ component=frontend
 
 echo install nginx
 dnf install nginx -y &>>$log_file
-if [ $? =eq 0 ] ; then
-  echo succes
-  else
-    echo failed
-    fi
+stat_check
 
 echo placing Expense config file in nginx
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-
+stat_check
 
 echo removing old nginx content
 rm -rf /usr/share/nginx/html/* &>>$log_file
+stat_check
+
 cd /usr/share/nginx/html
 
 download_and_extract
 
 echo starting nginx service
 systemctl enable nginx &>>$log_file
-
 systemctl restart nginx &>>$log_file
+stat_check
